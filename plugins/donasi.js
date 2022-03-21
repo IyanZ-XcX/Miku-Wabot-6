058efef094706f320f4a11f852fea216c8bcb97a
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-21 12:39:44 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,26 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ PULSA : 0857 4234 4873
-├ DANA,GOPAY,OVO : 0857 4234 4873
-└────
+_*PAYMENT RELLZ :*_
+╭─ *GopayOnly*
+╰ *➤* *https://ibb.co/4jYrTJP*
+╭─ *DanaOnly*
+╰ *➤* *085823437696*
+╭─ *Qris All Pay*
+╰ *➤* *https://tinyurl.com/y9nbgonc*
+
+*ATAS NAMA PAYMENT*
+*- GOPAY : R E L L Z*
+*- Dana : Farell*
+*- All Pay : CHILZ STORE*
+
+
+*IKUTI TUTORIAL INI*
+```- KLIK LINK PAY TRSEBUT
+- SKRENSHOOT
+- SCAN 
+- ISI NOM
+- TF 
+- DONE```
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
