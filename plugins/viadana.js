ee520e835f4b6ac9b4fee53d45a891e4a510550d
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-21 12:48:21 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,30 +1,25 @@
 let handler = async (m, { conn }) => {
-	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via DANA
-╠➥ Ke nomer ini
-║➥ 0857 4234 4873
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 ${namabot} 〙 ═`.trim(), m)
+	conn.reply(m.chat, `_*PAYMENT RELLZ :*_
+╭─ *GopayOnly*
+╰ *➤* *https://ibb.co/4jYrTJP*
+╭─ *DanaOnly*
+╰ *➤* *085823437696*
+╭─ *Qris All Pay*
+╰ *➤* *https://tinyurl.com/y9nbgonc*
+
+*ATAS NAMA PAYMENT*
+*- GOPAY : R E L L Z*
+*- Dana : Farell*
+*- All Pay : CHILZ STORE*
+
+
+*IKUTI TUTORIAL INI*
+- KLIK LINK PAY TRSEBUT
+- SKRENSHOOT
+- SCAN 
+- ISI NOM
+- TF 
+- DONE`.trim(), m)
 }
 
 handler.command = /^viadana$/i
