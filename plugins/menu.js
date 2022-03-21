989a996856a65828c924096ce3bfa9b644451f5e
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-21 12:52:55 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -162,7 +162,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send3ButtonImg(m.chat, await (await fetch(image)).buffer(), `*Hi!, ${name} ${ucapan()}*\n*I'am ᴍɪᴋᴜ-ʙᴏᴛ*`
+    await conn.send3ButtonImg(m.chat, await (await fetch(image)).buffer(), `*Hi!, ${name} ${ucapan()}*`
 , text.trim(), '👑 Owner Bot 👑', `,owner`, '📄 Rules 📄', '.rules', '🤑 Donasi 🤑', '.donasi', m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
