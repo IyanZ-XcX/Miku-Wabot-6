597423e19c976e7473b0515a2e1546061621d042
IyanZ-XcX <101798961+IyanZ-XcX@users.noreply.github.com>
2022-03-21 12:51:14 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,20 +1,26 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 15000*
-╠➥ *Permanen* : *Rp 25000*
-╠➥ *Premium* :   *Rp 10000*
-╠➥ *Sc Bot* :        *Masih Beta*
-║
-╠═〘 PEMBAYARAN 〙 ═
-╠➥ Dana, Dan Pulsa
-║
-╠═ Tertarik Untuk Beli Bot Ini?
-╠➥Ketuk Tombol Di Bawah Ya
-║
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
+	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `_*PAYMENT RELLZ :*_
+╭─ *GopayOnly*
+╰ *➤* *https://ibb.co/4jYrTJP*
+╭─ *DanaOnly*
+╰ *➤* *085823437696*
+╭─ *Qris All Pay*
+╰ *➤* *https://tinyurl.com/y9nbgonc*
+
+*ATAS NAMA PAYMENT*
+*- GOPAY : R E L L Z*
+*- Dana : Farell*
+*- All Pay : CHILZ STORE*
+
+
+*IKUTI TUTORIAL INI*
+- KLIK LINK PAY TRSEBUT
+- SKRENSHOOT
+- SCAN 
+- ISI NOM
+- TF 
+- DONE`.trim(), footer, 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
 }
 handler.help = ['sewabot']
 handler.tags = ['main']
